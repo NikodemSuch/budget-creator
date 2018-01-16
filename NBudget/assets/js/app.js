62cbb4b0d093dcb811e6fcc430715cef23486d66
Nikodem Such <nikodem.such@gmail.com>
2018-01-16 15:11:09 +0100

    Webpack Encore Setup

---
+++ NBudget/assets/js/app.js
@@ -0,0 +1,4 @@
+require('bootstrap-sass');
+require('../css/app.scss');
+require('font-awesome/scss/font-awesome.scss');
+var $ = require('jquery');
---
+++ NBudget/webpack.config.js
@@ -0,0 +1,26 @@
+var Encore = require('@symfony/webpack-encore');
+
+Encore
+
+    .setOutputPath('web/build/')
+    .setPublicPath('/build')
+
+    .addEntry('app', './assets/js/app.js')
+    .addStyleEntry('global', './assets/css/global.scss')
+
+    // allow sass/scss files to be processed
+    .enableSassLoader()
+
+    // allow legacy applications to use $/jQuery as a global variable
+    .autoProvidejQuery()
+
+    .enableSourceMaps(!Encore.isProduction())
+
+    // empty the outputPath dir before each build
+    .cleanupOutputBeforeBuild()
+
+    // show OS notifications when builds finish/fail
+    .enableBuildNotifications()
+;
+
+module.exports = Encore.getWebpackConfig();
