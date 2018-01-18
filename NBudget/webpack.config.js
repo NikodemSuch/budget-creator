09ecae4b739af9520777f86dde48cde6796a3743
Nikodem Such <nikodem.such@gmail.com>
2018-01-18 12:46:41 +0100

    Changed filenames for external css/js dependencies

--- NBudget/webpack.config.js
+++ NBudget/webpack.config.js
@@ -5,8 +5,8 @@ Encore
     .setOutputPath('web/build/')
     .setPublicPath('/build')
 
-    .addEntry('app', './assets/js/bootstrap+jquery.js')
-    .addStyleEntry('global', './assets/css/boostrap+fa.scss')
+    .addEntry('app', './assets/js/vendor.js')
+    .addStyleEntry('global', './assets/css/vendor.scss')
     .addStyleEntry('global', './assets/css/global.scss')
 
     // allow sass/scss files to be processed
