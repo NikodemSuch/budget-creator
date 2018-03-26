dbf0593b65530400a85c9b691cd82bbbdc259e16
Nikodem Such <nikodem.such@gmail.com>
2018-03-26 12:20:22 +0200

    Fixed the fix, and made it more readable

--- src/AppBundle/Twig/MoneyExtension.php
+++ src/AppBundle/Twig/MoneyExtension.php
@@ -27,11 +27,17 @@ class MoneyExtension extends \Twig_Extension
         $fmt = new \NumberFormatter($locale, \NumberFormatter::DECIMAL);
         $decimalPoint = $fmt->getSymbol(\NumberFormatter::DECIMAL_SEPARATOR_SYMBOL);
 
-        if (!$amount) {
-            return "0${decimalPoint}00";
+        if ($amount < 100 && $amount > -100) {
+        
+            if ($amount < 10 && $amount > -10) {
+                $amount = substr_replace($amount, '00' , -1 , 0);
+            }
+
+            else {
+                $amount = substr_replace($amount, '0' , -2 , 0);
+            }
         }
 
-        $amountStr = str_pad($amount, 3, '0', STR_PAD_LEFT);
-        return substr_replace($amountStr, $decimalPoint, -2 , 0);
+        return (null == $amount) ? "0${decimalPoint}00" : substr_replace($amount, $decimalPoint, -2 , 0);
     }
 }
