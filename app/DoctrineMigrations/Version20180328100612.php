86e17b2a42ccb8bdf6da9f7f82fdf6c33fb92b91
Nikodem Such <nikodem.such@gmail.com>
2018-03-28 12:08:27 +0200

    Deleted unnecessary migrations.

--- app/DoctrineMigrations/Version20180328100612.php
+++ app/DoctrineMigrations/Version20180328100612.php
@@ -8,14 +8,14 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20180326165435 extends AbstractMigration
+class Version20180328100612 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE notification ADD url LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:json)\'');
+        $this->addSql('ALTER TABLE notification ADD route_name VARCHAR(200) DEFAULT NULL, ADD route_parameters LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:json)\'');
     }
 
     public function down(Schema $schema)
@@ -23,6 +23,6 @@ class Version20180326165435 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE notification DROP url');
+        $this->addSql('ALTER TABLE notification DROP route_name, DROP route_parameters');
     }
 }
